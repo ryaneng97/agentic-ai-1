028ce3ccc8c4814d2451c9a96a3ffdb2372a8b65
Ryan English <ryanenglish@Ryans-MacBook-Pro-3.local>
2025-12-16 21:20:01 +0000

    initial main commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+from langchain.agents import create_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+#Load API Key
+API_KEY = os.environ["GOOGLE_API_KEY"]
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-pro",
+    temperature=0.2,
+    api_key=API_KEY
+)
+                     
+def get_weather(city: str) -> str:
+    """Get weather for a given city"""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model=llm,
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+response = agent.invoke({"messages": [{"role": "user", "content": "What is the weather in Derry, Northern Ireland?"}]})
+
+
+print(response)
